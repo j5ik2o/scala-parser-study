0843b90a74fb04f7436874de7e74abdb41beaef0
Junichi Kato <junichi.kato@gree.net>
2013-12-01 14:08:35 +0900

    added license.

--- src/test/scala/com/github/j5ik2o/sps/parser/Q4ParserSpec.scala
+++ src/test/scala/com/github/j5ik2o/sps/parser/Q4ParserSpec.scala
@@ -1,8 +1,23 @@
+/*
+ * Copyright 2013 @j5ik2o.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND,
+ * either express or implied. See the License for the specific language
+ * governing permissions and limitations under the License.
+ */
 package com.github.j5ik2o.sps.parser
 
 import org.specs2.mutable.Specification
 import java.io.StringReader
-import com.github.j5ik2o.sps.model.{AddExpr, SubExpr, MinusExpr, ValueExpr}
+import com.github.j5ik2o.sps.model.{AddExpr, ValueExpr}
 
 class Q4ParserSpec extends Specification with SpecSupport {
 
@@ -16,10 +31,10 @@ class Q4ParserSpec extends Specification with SpecSupport {
       process("10") must_==(ValueExpr(10), 10)
     }
     "be the same as -10" in {
-      process("10 + 20") must_==(AddExpr(ValueExpr(10),ValueExpr(20)),30)
+      process("10 + 20") must_==(AddExpr(ValueExpr(10), ValueExpr(20)), 30)
     }
     "be the same as -10" in {
-      process("10 + 20 + 30 + 40") must_==(AddExpr(AddExpr(AddExpr(ValueExpr(10),ValueExpr(20)),ValueExpr(30)),ValueExpr(40)),100)
+      process("10 + 20 + 30 + 40") must_==(AddExpr(AddExpr(AddExpr(ValueExpr(10), ValueExpr(20)), ValueExpr(30)), ValueExpr(40)), 100)
     }
   }
 }
